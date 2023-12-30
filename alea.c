9790be583644405a6d61ad957668c86e7031caaf
manteiga25 <80206082+manteiga25@users.noreply.github.com>
2023-12-30 19:15:07 +0000

    optimized memory utilization

--- alea.c
+++ alea.c
@@ -11,11 +11,11 @@ int main() {
     // Define a semente com base no tempo atual
     srand(time(NULL));
     puts("Escreva 10 números entre 1 e 100");
-    for (unsigned short i = 0; i < 10; i++) {
+    for (unsigned char i = 0; i < 10; i++) {
         num_esc[i] = rand() % 100 + 1;
     }
     
-    for (unsigned short i = 0; i < 10; i++) {
+    for (unsigned char i = 0; i < 10; i++) {
         printf("Escreva o %huº número: ", i + 1);
         scanf("%hu", &nums[i]);
         if (nums[i] > 100 || nums[i] <= 0) {
@@ -23,7 +23,7 @@ int main() {
         i--;
         }
         if (i) {
-        for (unsigned short check = 0; check < i; check++) {
+        for (unsigned char check = 0; check < i; check++) {
         if (check != i)
         if (nums[i] == nums[check]) {
         puts("Número repetido, escreva outro número");
@@ -35,17 +35,17 @@ int main() {
     }
     
 #if debug
-    for (int i = 0; i < 10; i++) {
+    for (unsigned char i = 0; i < 10; i++) {
       printf("\n%hd\n", num_esc[i]);
     }
 #endif
     
     puts("começou");
     
-    for (unsigned short i = 1; i <= 100; i++) {
+    for (unsigned char i = 1; i <= 100; i++) {
       bool aserto = false;
-      for (unsigned short j = 0; j < 10; j ++) {
-          for (unsigned short k = 0; k < 10; k++)
+      for (unsigned char j = 0; j < 10; j ++) {
+          for (unsigned char k = 0; k < 10; k++)
           if (i == num_esc[j] && num_esc[j] == nums[k]) {
           printf(" X");
           aserto = true;
